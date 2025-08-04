1ef321699cde50ed308d7b7d2626ac1149492b03
Blanc <thetruedefenders4@gmail.com>
2025-08-04 13:27:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 from PIL import Image, ImageEnhance, ImageFilter
 import io
 import os
-# opencv-python
 import cv2
 import plotly.graph_objects as go
 import plotly.express as px
